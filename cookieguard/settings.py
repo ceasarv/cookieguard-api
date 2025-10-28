de7a01a76c40ab686d5ca60492ea176c724119b4
ceasarv <ceasar.vanbuskirk@gmail.com>
2025-10-28 00:35:03 -0700

    Update db config

--- cookieguard/settings.py
+++ cookieguard/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,13 +111,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cookieguard.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+if ENV == "production":
+	DATABASES = {
+		"default": dj_database_url.config(
+			default=os.getenv("DATABASE_URL"),
+			conn_max_age=600,
+			ssl_require=True
+		)
 	}
-}
+else:
+	DATABASES = {
+		"default": {
+			"ENGINE": "django.db.backends.sqlite3",
+			"NAME": BASE_DIR / "db.sqlite3",
+		}
+	}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
