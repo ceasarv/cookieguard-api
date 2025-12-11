1c6ec5b806377a2668594a673ac94a9319010028
ceasarv <ceasar.vanbuskirk@gmail.com>
2025-12-10 21:36:14 -0800

    Update test embed keys

--- banners/views.py
+++ banners/views.py
@@ -97,7 +97,7 @@ def banner_metadata(request, embed_key: str):
 
 
 # Used to bypass subscription checks during testing
-TEST_EMBED_KEYS = ['X9oOsVr4IYqLTSsjqaEtZg0J9OuYsByF', '3-uK4ofsHmlqNq0LIsZaTmY37OJN_HmD']
+TEST_EMBED_KEYS = ['X9oOsVr4IYqLTSsjqaEtZg0J9OuYsByF', '3-uK4ofsHmlqNq0LIsZaTmY37OJN_HmD', 'GoTfwqm04veYlewWPrrkd8BJ7GHb3uZ8']
 
 
 @permission_classes([AllowAny])
